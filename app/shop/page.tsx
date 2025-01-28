c71cc5b2832153312283bcdc4a1b4b64f9085ae9
KulsumShaikh16 <160030040+KulsumShaikh16@users.noreply.github.com>
2025-01-28 22:24:56 +0500

    Normalize line endings

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -3,8 +3,6 @@ import Header from '@/components/Header'
 import Shophero from '@/components/shophero'
 import ProductListing from '@/components/ProductListing'
 import Shopcta from '@/components/Shopcta'
-// import Filter from '@/components/Filter'
-// import Buttonpg from '@/components/Pagination'
 import Footer from '@/components/Footer'
 
 const Shop = () => {
@@ -12,9 +10,9 @@ const Shop = () => {
     <div>
       <Header bgColor="#FFFFFF"/>
       <Shophero />
-      {/* <Filter /> */}
+      
       <ProductListing />
-      {/* <Buttonpg /> */}
+      
       <Shopcta />
       <Footer />
       
